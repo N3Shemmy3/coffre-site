8024efef59b7b3090c3f7d1e9bcc8a07f7e09c81
n3shemmy3 <67279072+N3Shemmy3@users.noreply.github.com>
2026-01-15 15:27:25 +0200

    add modules, css and fix tailwind bugs by installing native packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+import tailwindcss from "@tailwindcss/vite";
 
-  modules: [
-    '@nuxt/fonts',
-    '@nuxt/hints',
-    '@nuxt/icon',
-    '@nuxt/image'
-  ]
+export default defineNuxtConfig({
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+    css: [
+        '~/assets/css/app.css',
+    ],
+    modules: [
+        '@nuxt/fonts',
+        '@nuxt/hints',
+        '@nuxt/icon',
+        '@nuxt/image',
+        'lenis/nuxt'
+    ],
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ],
+    },
 })
